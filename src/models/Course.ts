b9c76eb0a4d31ff3eaeb9de2f702623b760daac2
rohitrrr345 <rrajput2060@gmail.com>
2024-10-27 18:24:35 +0530

    Added Course Models

--- src/models/Course.ts
+++ src/models/Course.ts
@@ -2,13 +2,63 @@ import mongoose from 'mongoose';
 const schema=new mongoose.Schema({
 title:{
     type:String,
+    required:[true,"title is required"],
+    minLength:[5,"title must of length of 5 characters"],
+},
+author:{
+    type:String,
     required:true,
+
 },
 description:{
     type:String,
-    required:true,
+    required:[true,"description is required"],
+    minLength:[5,"description must of length of 5 characters"],
 },
 
+pdfs:[
+ { 
+     title:{
+    type:String,            
+    required:[true,"title is required"],
+
+   },
+   description:{
+       type:String,            
+       required:[true,"description is required"],
+
+   },
+   documentArray:{
+    public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+   }
 }
+],
+numberOfDocuments:{ 
+    type:Number,
+    default:0,
+},
+
+NumberOfDownloads:{
+    type:Number,
+    default:0,
+},
+category:{
+    type:String,
+    required:[true,"category is required"],
+},
+}
+,
+{
+    timestamps:true,
+}
+
+
 );
 export const Course=mongoose.model('Course',schema);
